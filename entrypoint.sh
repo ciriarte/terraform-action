485d895fab8af0f7f3f7ffc6e22ae6d2fc148394
Carlos Iriarte <ciriarte@users.noreply.github.com>
2021-04-17 07:45:05 -0700

    fix: run implicit get
    Authored-by: Carlos Iriarte <ciriarte@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,5 @@
 #! /usr/bin/env bash
 
-: "${OUTPUT_PATH:-$PWD}"
-
 ENV_NAME=$1
 TERRAFORM_SOURCE=$2
 SOURCE=$3
@@ -10,12 +8,9 @@ OVERRIDE_FILES=$5
 DELETE_ON_FAILURE=$6
 VARS=$7
 
-if [[ -n $8 ]]; then
-  OUTPUT_PATH=$8
-  mkdir -p "$OUTPUT_PATH"
-fi
+tmp_dir=$(mktemp -d)
 
-/opt/resource/out "$PWD" <<JSON
+/opt/resource/out "$PWD" > "${tmp_dir}/check" <<JSON
 {
   "params": {
     "env_name": "$ENV_NAME",
@@ -29,15 +24,13 @@ fi
 }
 JSON
 
+VERSION=$(jq .version "${tmp_dir}/check")
+
 /opt/resource/in "$PWD" <<JSON
 {
+  "version": $VERSION
   "params": {
     "env_name": "$ENV_NAME",
-    "terraform_source": "$TERRAFORM_SOURCE",
-    "var_files": $VAR_FILES,
-    "override_files": $OVERRIDE_FILES,
-    "delete_on_failure": $DELETE_ON_FAILURE,
-    "vars": $VARS
   },
   "source": $SOURCE
 }
